e6b16be1742085ae8ef8c94564c347b0286933b9
JoseManuel <manuel.paredes@grupotimex.onmicrosoft.com>
2023-09-12 14:23:36 -0600

    setup retrofit

---
+++ app/src/main/java/com/pepivsky/users/NetworkModule.kt
@@ -0,0 +1,29 @@
+package com.pepivsky.users
+
+import com.google.gson.GsonBuilder
+//import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+//import kotlinx.serialization.json.Json
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+//import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class) // scope para toda la app
+class NetworkModule {
+
+    // anotaciones para proveer un singleton, es decir que hay una unica instancia
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://jsonplaceholder.typicode.com")
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+    }
+}
